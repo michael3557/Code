fbd34b6f65880f05c452eec510b86665ba3625ef
Michael Davis <michaeld3557@gmail.com>
2018-04-01 16:09:33 -0700

    Got js to work for scree size

--- DSP/codepen.js
+++ DSP/codepen.js
@@ -5,9 +5,31 @@ $('.menu-toggle').click(function() {
   
 })
 
-/*
-$(document).ready(function(){
-  $('.site-nav').click(function(){
-    $('.site-nav').toggleClass('open')
-  })
-})*/
+
+window.addEventListener("resize", myFunction);
+var last = 0;
+var x = 0;
+var y = 0;
+var z = 0;
+
+function myFunction() {
+ 
+   let txt = window.innerWidth;
+   
+      if(txt <= 1200 &&  x === 0 ){
+       $('.menu-toggle').toggleClass('active', 500);
+       $('.site-nav').toggleClass('active', 500);
+      x++;
+      y = 0
+      }
+      else if(txt > 1200 && y === 0 ){
+          $('.menu-toggle').toggleClass('active', 500);
+          $('.site-nav').toggleClass('active', 500);
+          y++;
+          x = 0;
+      }
+
+ document.getElementById("demo").innerHTML = "z: " + z + "  x: " + x + "  y: " + y + "  new: " + txt + "  old: " + last +  "  class: " + document.getElementsByTagName("BODY")[0].className;
+ last = txt;
+ z++;
+}
