a3c3c623b9901a583e7c4d76f476d6d821bce2d8
Michael Davis <michaeld3557@gmail.com>
2018-04-29 17:09:51 -0700

    Lesson 10 done upto last finnishing touches

--- swd101/Lesson7/script7ho2.js
+++ swd101/Lesson7/script7ho2.js
@@ -1,7 +1,9 @@
 function clickit(){
   const xhttp = new XMLHttpRequest()
   xhttp.onreadystatechange = function(){
+             console.log(this.status);
    if(this.readyState == 4 && this.status == 200){
+         console.log(this.status);
      document.getElementById("text").innerHTML = this.responseText;
      document.getElementById("foot").style.display = "block";
   }
